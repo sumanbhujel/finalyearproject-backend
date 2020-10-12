76d907e3c0662fcd406fa8f72f5259410fc64288
sumanbhujel <xnamus@outlook.com>
2020-10-12 10:22:40 +0545

    initial commit

---
+++ config/dbConfig.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb://127.0.0.1:27017/text-recognizer", {
+    useNewUrlParser: true, 
+    useCreateIndex: true, 
+    useUnifiedTopology:true,
+    useFindAndModify: false
+}).then(() => console.log("Database connected successfully..."))
+    .catch((error) => console.log(error));
